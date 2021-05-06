6d4a46d0b2931b9c8fe6e0c8746b5790f837fa59
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-06 15:12:39 +0200

    remove android for now

--- android/src/main/java/com/visioncameraimagelabeller/VisionCameraImageLabellerPackage.java
+++ android/src/main/java/com/visioncameraimagelabeller/VisionCameraImageLabellerPackage.java
@@ -1,28 +1,4 @@
 package com.visioncameraimagelabeler;
 
-import androidx.annotation.NonNull;
-
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class VisionCameraImageLabelerPackage implements ReactPackage {
-    @NonNull
-    @Override
-    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new VisionCameraImageLabelerModule(reactContext));
-        return modules;
-    }
-
-    @NonNull
-    @Override
-    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-        return Collections.emptyList();
-    }
+public class VisionCameraImageLabelerPackage {
 }
--- example/android/app/src/main/java/com/example/visioncameraimagelabeller/MainApplication.java
+++ example/android/app/src/main/java/com/example/visioncameraimagelabeller/MainApplication.java
@@ -2,7 +2,6 @@ package com.example.visioncameraimagelabeler;
 
 import android.app.Application;
 import android.content.Context;
-import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -10,7 +9,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.visioncameraimagelabeler.VisionCameraImageLabelerPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -23,12 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for VisionCameraImageLabelerExample:
-          // packages.add(new MyReactNativePackage());
-          packages.add(new VisionCameraImageLabelerPackage());
-          return packages;
+          return new ArrayList<ReactPackage>();
         }
 
         @Override
