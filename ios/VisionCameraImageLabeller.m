9d6b9a307840c0ca71f251020874c06b47cb614a
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-06 14:50:36 +0200

    create

--- ios/VisionCameraImageLabeller.m
+++ ios/VisionCameraImageLabeller.m
@@ -1,19 +1,56 @@
-#import "VisionCameraImageLabeller.h"
+//
+//  VisionCameraImageLabeller.m
+//  VisionCameraExample
+//
+//  Created by Marc Rousavy on 06.05.21.
+//
 
-@implementation VisionCameraImageLabeller
+#import <VisionCamera/FrameProcessorPlugin.h>
+@import Foundation;
+@import AVFoundation;
+@import MLKitVision;
+@import MLKit;
+@import MLKitImageLabeling;
+@import MLKitImageLabelingCommon;
 
-RCT_EXPORT_MODULE()
+// Example for an Objective-C Frame Processor plugin
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
+@interface QRCodeFrameProcessorPluginObjC : NSObject
 
-  resolve(result);
++ (MLKImageLabeler*) labeler;
+
+@end
+
+@implementation QRCodeFrameProcessorPluginObjC
+
++ (MLKImageLabeler*) labeler {
+  static MLKImageLabeler* labeler = nil;
+  if (labeler == nil) {
+    MLKImageLabelerOptions* options = [[MLKImageLabelerOptions alloc] init];
+    labeler = [MLKImageLabeler imageLabelerWithOptions:options];
+  }
+  return labeler;
 }
+
+static inline id exampleObjC___scanQRCodes(CMSampleBufferRef buffer, NSArray* arguments) {
+  CFTimeInterval startTime = CACurrentMediaTime();
+  
+  MLKVisionImage *image = [[MLKVisionImage alloc] initWithBuffer:buffer];
+  image.orientation = UIImageOrientationRight; // <-- TODO: is mirrored?
+  
+  NSError* error;
+  NSArray<MLKImageLabel*>* labels = [[QRCodeFrameProcessorPluginObjC labeler] resultsInImage:image error:&error];
+  
+  NSMutableArray* results = [NSMutableArray arrayWithCapacity:labels.count];
+  for (MLKImageLabel* label in labels) {
+    [results addObject:@{ @"label": label.text, @"confidence": [NSNumber numberWithFloat:label.confidence] }];
+  }
+  
+  CFTimeInterval elapsedTime = CACurrentMediaTime() - startTime;
+  NSLog(@"Native took: %fms", elapsedTime * 1000.0);
+  return results;
+}
+
+VISION_EXPORT_FRAME_PROCESSOR(exampleObjC___scanQRCodes)
 
 @end
