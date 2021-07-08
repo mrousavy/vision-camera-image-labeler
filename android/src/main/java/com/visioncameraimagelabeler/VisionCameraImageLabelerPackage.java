53efa9c16b3a6ce8d0c1c12ff8ea9ee92c54f3a9
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-08 11:39:50 +0200

    fix: Null Package error

--- android/src/main/java/com/visioncameraimagelabeler/VisionCameraImageLabelerPackage.java
+++ android/src/main/java/com/visioncameraimagelabeler/VisionCameraImageLabelerPackage.java
@@ -8,6 +8,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
 
+import java.util.Collections;
 import java.util.List;
 
 public class VisionCameraImageLabelerPackage implements ReactPackage {
@@ -16,13 +17,13 @@ public class VisionCameraImageLabelerPackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(@NonNull @org.jetbrains.annotations.NotNull ReactApplicationContext reactContext) {
     FrameProcessorPlugin.register(new VisionCameraImageLabelerPlugin());
-    return null;
+    return Collections.emptyList();
   }
 
   @NonNull
   @org.jetbrains.annotations.NotNull
   @Override
   public List<ViewManager> createViewManagers(@NonNull @org.jetbrains.annotations.NotNull ReactApplicationContext reactContext) {
-    return null;
+    return Collections.emptyList();
   }
 }
