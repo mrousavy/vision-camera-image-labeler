14a0cdb03efe30c52526d7aea60d75f9fa1aad81
Bartosz Kaszubowski <gosimek@gmail.com>
2021-08-20 12:04:53 +0200

    Update example/src/App.tsx
    Co-authored-by: Marc Rousavy <marcrousavy@hotmail.com>

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -23,7 +23,7 @@ export default function App() {
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet';
     const labels = labelImage(frame);
-    _log(`Labels: ${labels.join(', ')}`);
+    console.log(`Labels: ${labels.join(', ')}`);
   }, []);
 
   return (
