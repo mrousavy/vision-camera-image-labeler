670f5d5b783f33ffcdbec582cd7bbba4bc57efc3
Test User <gosimek@gmail.com>
2021-08-20 14:18:33 +0200

    log message tweak

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -23,7 +23,7 @@ export default function App() {
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet';
     const labels = labelImage(frame);
-    console.log(`Labels: `, labels);
+    console.log('Labels', labels);
   }, []);
 
   return (
